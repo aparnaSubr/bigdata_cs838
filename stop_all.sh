2d538a5f5d2d73191bd450303420e56601a99418
Aparna Subramanian <aparna.subr15@gmail.com>
2016-09-14 16:37:15 -0500

    scripts to start and stop vms

---
+++ stop_all.sh
@@ -0,0 +1,4 @@
+xterm -hold -e azure vm deallocate group14 vm1 &
+xterm -hold -e azure vm deallocate group14 vm2 &
+xterm -hold -e azure vm deallocate group14 vm3 &
+xterm -hold -e azure vm deallocate group14 vm4 &

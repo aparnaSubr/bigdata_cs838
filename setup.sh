fa3771ade7abe24111da5d9ad9cc6f7252e2ad24
Aparna Subramanian <aparna.subr15@gmail.com>
2016-09-14 21:28:46 -0500

    Script to create directory structure

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
+echo "Creating directories"
 cd
-sudo apt-get update --fix-missing
-sudo apt-get -y install openjdk-7-jdk
-sudo apt-get -y install pdsh
+#sudo apt-get update --fix-missing
+#sudo apt-get -y install openjdk-7-jdk
+#sudo apt-get -y install pdsh
 sudo chown ubuntu:ubuntu /mnt
 sudo chown ubuntu:ubuntu /workspace
 cd /home/ubuntu
@@ -11,4 +12,13 @@ mkdir workload
 mkdir /mnt/logs
 mkdir /mnt/logs/apps
 mkdir /mnt/logs/hadoop
-mkdir /workspace
+#mkdir /workspace
+mkdir /workspace/storage
+mkdir /workspace/storage/data
+mkdir /workspace/storage/data/local
+mkdir /workspace/storage/data/local/nm
+mkdir /workspace/storage/data/local/tmp
+mkdir /workspace/storage/hdfs
+mkdir /workspace/storage/hdfs/hdfs_dn_dirs
+mkdir /workspace/storage/hdfs/hdfs_nn_dir
+echo "Done"

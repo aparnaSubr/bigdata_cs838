4c73445402eaf39ada3778f397505eecdc56a2a5
adithya.bhatp <adithya.bhatp2@gmail.com>
2016-09-23 16:28:22 -0500

    adding reset access script

---
+++ vms_resetAccess.bat
@@ -0,0 +1,5 @@
+
+start call azure vm reset-access -g group14 -n vm1 -r -u ubuntu -p Pass123123$
+start call azure vm reset-access -g group14 -n vm2 -r -u ubuntu -p Pass123123$
+start call azure vm reset-access -g group14 -n vm3 -r -u ubuntu -p Pass123123$
+start call azure vm reset-access -g group14 -n vm4 -r -u ubuntu -p Pass123123$

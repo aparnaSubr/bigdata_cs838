29ac90deac4f45247c47594ac95c3622b470f6a0
Aparna Subramanian <aparna.subr15@gmail.com>
2016-09-23 18:10:41 -0500

    reset access sh

---
+++ vms_reset_access.sh
@@ -0,0 +1,4 @@
+xterm -hold -e azure vm reset-access -g group14 -n vm1 -r -u ubuntu -p Pass123123$ &
+xterm -hold -e azure vm reset-access -g group14 -n vm2 -r -u ubuntu -p Pass123123$ &
+xterm -hold -e azure vm reset-access -g group14 -n vm3 -r -u ubuntu -p Pass123123$ &
+xterm -hold -e azure vm reset-access -g group14 -n vm4 -r -u ubuntu -p Pass123123$ &

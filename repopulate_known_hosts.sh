6178b4cea14ca95452b7e73d8606cc0fd9cfec69
Aparna Subramanian <aparna.subr15@gmail.com>
2016-09-23 21:30:44 +0000

    Add ssh known_hosts workaround

---
+++ repopulate_known_hosts.sh
@@ -0,0 +1,9 @@
+rm -f ~/.ssh/known_hosts
+ssh -o StrictHostKeyChecking=no vm4 << HERE1
+HERE1
+ssh -o StrictHostKeyChecking=no vm3 << HERE2
+HERE2
+ssh -o StrictHostKeyChecking=no vm2 << HERE3
+HERE3
+ssh -o StrictHostKeyChecking=no vm1 << HERE4
+HERE4

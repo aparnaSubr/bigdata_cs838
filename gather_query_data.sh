13e7aea44ebfe6f48b09e07a245f0a2dd2215016
Aparna Subramanian <aparna.subr15@gmail.com>
2016-09-27 03:26:16 +0000

    Initial script to collect data

---
+++ gather_query_data.sh
@@ -0,0 +1,21 @@
+(hive --hiveconf hive.execution.engine=tez --hiveconf hive.tez.container.size=4800 --hiveconf hive.tez.java.opts=-Xmx4400m -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_tez1.out
+(hive --hiveconf hive.execution.engine=tez --hiveconf hive.tez.container.size=4800 --hiveconf hive.tez.java.opts=-Xmx4400m -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_tez2.out
+(hive --hiveconf hive.execution.engine=tez --hiveconf hive.tez.container.size=4800 --hiveconf hive.tez.java.opts=-Xmx4400m -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_tez3.out
+(hive --hiveconf hive.execution.engine=tez --hiveconf hive.tez.container.size=4800 --hiveconf hive.tez.java.opts=-Xmx4400m -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_tez4.out
+(hive --hiveconf hive.execution.engine=tez --hiveconf hive.tez.container.size=4800 --hiveconf hive.tez.java.opts=-Xmx4400m -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_tez5.out
+(hive --hiveconf hive.execution.engine=tez --hiveconf hive.tez.container.size=4800 --hiveconf hive.tez.java.opts=-Xmx4400m -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_tez6.out
+(hive --hiveconf hive.execution.engine=tez --hiveconf hive.tez.container.size=4800 --hiveconf hive.tez.java.opts=-Xmx4400m -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_tez7.out
+(hive --hiveconf hive.execution.engine=tez --hiveconf hive.tez.container.size=4800 --hiveconf hive.tez.java.opts=-Xmx4400m -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_tez8.out
+(hive --hiveconf hive.execution.engine=tez --hiveconf hive.tez.container.size=4800 --hiveconf hive.tez.java.opts=-Xmx4400m -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_tez9.out
+(hive --hiveconf hive.execution.engine=tez --hiveconf hive.tez.container.size=4800 --hiveconf hive.tez.java.opts=-Xmx4400m -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_tez0.out
+
+(hive --hiveconf hive.execution.engine=mr -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_mr1.out
+(hive --hiveconf hive.execution.engine=mr -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_mr2.out
+(hive --hiveconf hive.execution.engine=mr -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_mr3.out
+(hive --hiveconf hive.execution.engine=mr -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_mr4.out
+(hive --hiveconf hive.execution.engine=mr -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_mr5.out
+(hive --hiveconf hive.execution.engine=mr -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_mr6.out
+(hive --hiveconf hive.execution.engine=mr -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_mr7.out
+(hive --hiveconf hive.execution.engine=mr -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_mr8.out
+(hive --hiveconf hive.execution.engine=mr -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_mr9.out
+(hive --hiveconf hive.execution.engine=mr -f sample-queries-tpcds/query21.sql --database tpcds_text_db_1_50) 2> output/query21/query21_mr0.out
